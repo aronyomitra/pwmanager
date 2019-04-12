55d6ed099ebaae4d83b0d38488434ac777ce9fd1
Aronyo Mitra <aronyomitra@gmail.com>
2019-04-12 22:37:03 +0530

    Add default login condition
    Remove leading and trailing whitespace ('\t','\r','\n' etc) from file 
    data

--- login.py
+++ login.py
@@ -2,9 +2,11 @@ import pyperclip, time
 from pynput.keyboard import Key, Controller
 
 keyboard = Controller()
+
 def login(acc, credList):
-    if credList[-1][-1] == '\n':
-        credList[-1] = credList[-1][0:-1]
+
+    #Remove leading and trailing whitespace
+    credList[-1] = credList[-1].strip()
 
     #Checking acc against preset accounts
     if acc == 'fb':
@@ -16,5 +18,10 @@ def login(acc, credList):
         time.sleep(2)
         keyboard.type(credList[1] + '\r')
     elif acc == 'steam':
+        time.sleep(3)
+        keyboard.type(credList[0] + '\t' + credList[1] + '\r')
+
+    #Default login
+    else:
         time.sleep(3)
         keyboard.type(credList[0] + '\t' + credList[1] + '\r')
