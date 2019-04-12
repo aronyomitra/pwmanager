a17a62fd76b572700dad8a60937a668affdf29ab
Aronyo Mitra <aronyomitra@gmail.com>
2019-04-12 22:18:47 +0530

    Fix broken setup script

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 import os
-exists = os.path.isFile(r"data\data.txt")
-if exists == False:
-    file = open (r"data\data.txt", 'w')
+exists = True
+try:
+    file = open ("data\data.txt", 'r')
+except FileNotFoundError:
+    exists = False
+    os.mkdir('data')
+    file = open ("data\data.txt", 'w')
     file.write("fb:name@gmail.com,password")
     file.write("gmail:username,password")
     file.close()
     print ("Created new file!")
-else:
+
+if exists == True:
     print ("File already exists!")
     print ("Make sure to follow the format:")
     print ("account:username,password")
-
-file.close()
+    file.close()
